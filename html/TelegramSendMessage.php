cace3e074ca0fd9cb5004abd5643d58e7e5a952f
freeman <4reeman>
2022-10-26 02:30:20 +0300

    qwe

--- html/TelegramSendMessage.php
+++ html/TelegramSendMessage.php
@@ -2,7 +2,7 @@
 
 Class TelegramSendMessage {
 
-    final const TGTOKEN = '5793129764:AAGR9DRRbMjBl4Byei70Sec6OiqAfuwdQRw';
+    final const TGTOKEN = '5705182372:AAFDEVyqyd6rn04PZjqn7iG5ZImRTBHBglg';
     final const URL = 'https://api.telegram.org/bot' . self::TGTOKEN;
 
     public function response($method, $parameters) {
